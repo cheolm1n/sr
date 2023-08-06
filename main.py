c63b8481afe6ae9091b5e67c4917382ac0d49209
iron.min <iron.min@kakaocorp.com>
2023-08-06 12:54:26 +0900

    openai 병령 요청 및 결과파일 쓰기 구현

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import argparse
 import math
+from concurrent.futures import ThreadPoolExecutor
 
+import openai
 import tiktoken
+from tqdm import tqdm
 
-MODEL_NAME = "gpt-3.5-turbo-16k-0613"
-MAX_TOKEN = 4096 * 4
+# MODEL_NAME = "gpt-3.5-turbo-16k-0613"
+# MAX_TOKEN = 4096 * 4
 BUFFER = 50
+MAX_TOKENS = {
+    "gpt-4": 8192,
+    "gpt-4-0613": 8192,
+    "gpt-4-32k": 32768,
+    "gpt-4-32k-0613": 32768,
+    "gpt-3.5-turbo": 4096,
+    "gpt-3.5-turbo-16k": 16384,
+    "gpt-3.5-turbo-0613": 4096,
+    "gpt-3.5-turbo-16k-0613": 16384,
+    "text-davinci-003": 4097,
+    "text-davinci-002": 4097,
+    "code-davinci-002": 8001,
+    "text-curie-001": 2049,
+    "text-babbage-001": 2049,
+    "text-ada-001": 2049,
+    "davinci": 2049,
+    "curie": 2049,
+    "babbage": 2049,
+    "ada": 2049
+}
 
 
 # 파싱된 인자 출력
@@ -35,6 +58,41 @@ def get_file_info(string: str, encoding_name: str) -> dict:
         return {"count_tokens": count_tokens, "average": average, "total_line": total_line}
 
 
+def ask_openai(model_name, system_message, user_message):
+    response = openai.ChatCompletion.create(
+        model=model_name,
+        messages=[
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": user_message},
+        ]
+    )
+    return response['choices'][0]['message']['content']
+
+
+def ask_openai_parallel(model_name, system_user_pairs, output_file):
+    with ThreadPoolExecutor() as executor:
+        futures = {executor.submit(ask_openai, model_name, pair[0], pair[1]): i for i, pair in enumerate(system_user_pairs)}
+        results = [None] * len(futures)
+
+        with tqdm(total=len(futures), desc="Processing pairs", ncols=70) as pbar:
+            for future in futures:
+                results[futures[future]] = future.result()
+                pbar.update()
+
+        with open(output_file, 'w') as file:
+            for result in results:
+                file.write(result + '\n')
+
+
+def create_pairs_from_file(file_name, prompt, lines_per_pair):
+    with open(file_name, 'r') as file:
+        content = file.read().splitlines()
+
+    pairs = [(prompt, ' '.join(content[i:i + lines_per_pair])) for i in range(0, len(content), lines_per_pair)]
+
+    return pairs
+
+
 parser = argparse.ArgumentParser(description="This is a simple argument parser")
 
 # 인자 추가
@@ -42,18 +100,19 @@ parser.add_argument("inputs", type=str, nargs='+', help="Input to be printed out
 
 # 인자 파싱
 args = parser.parse_args()
+openai.api_key = args.inputs[2]
 
 if __name__ == '__main__':
     print_args(args)
 
-    prompt_token = count_tokens(args.inputs[0], MODEL_NAME)
+    prompt_token = count_tokens(args.inputs[0], args.inputs[1])
     print("# prompt token : " + str(prompt_token))
 
-    file_info = get_file_info(args.inputs[3], MODEL_NAME)
+    file_info = get_file_info(args.inputs[3], args.inputs[1])
 
     print("# file token : " + str(file_info))
 
-    available_token = math.floor((MAX_TOKEN - prompt_token) / 2 - BUFFER)
+    available_token = math.floor((MAX_TOKENS[args.inputs[1]] - prompt_token) / 2 - BUFFER)
     print("# available token : " + str(available_token))
 
     line_per_request = math.floor(available_token / file_info.get("average"))
@@ -62,10 +121,6 @@ if __name__ == '__main__':
     total_request = math.ceil(file_info.get("total_line") / line_per_request)
     print("# total request : " + str(total_request))
 
-# prompt
-# model
-# api key
-# file path
+    pairs = create_pairs_from_file(args.inputs[3], args.inputs[0], line_per_request)
 
-# available : (model_max_token - prompt_token_count) / 2 - buffer
-# request_count : total_line / available / average_token_per_line
+    ask_openai_parallel(args.inputs[1], pairs, "result_" + args.inputs[3])
